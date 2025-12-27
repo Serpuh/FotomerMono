e583af82113ab0967fb89eaf5eec4634f5c7ea33
Kol888 <sapr219@yandex.ru>
2025-12-27 18:28:13 +0300

    thread2

--- SettingsDlg.py
+++ SettingsDlg.py
@@ -1,42 +1,37 @@
 import sys
 import torch
+import os
+
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
 import numpy as np
 import utils3d
 import time
-import os
+
 import math
 from pathlib import Path
 
-import cv2
 import time
 
 path1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(path1)
 
-#from ..moge.model.v2 import MoGeModel
-#from model.v2 import MoGeModel
-#from ..moge import MoGeModel
 from moge.model.v2 import MoGeModel
-# from moge.model import import_model_class_by_version
-
 from moge.utils.io import save_glb, save_ply
 from moge.utils.vis import colorize_depth, colorize_normal
 
-# from moge.utils.geometry_numpy import depth_occlusion_edge_numpy
-
 from PySide6.QtWidgets import QDialog
-from PySide6.QtCore import QUrl, QRegularExpression, QTimer
+from PySide6.QtCore import QUrl, QRegularExpression, QTimer, QObject, Signal, Slot
 from PySide6.QtGui import QDesktopServices, QDoubleValidator, QRegularExpressionValidator, QTextCursor
 
 from ui_SettingsDlg import Ui_SettingsDlg
 
 from ui_form import Ui_MainWindow
 
-os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+
 project_root = Path(__file__).parent.parent
 sys.path.append(str(project_root))
-
+import cv2
 
 class SettingsDlg(QDialog):
     def __init__(self, parent):
@@ -61,13 +56,11 @@ class SettingsDlg(QDialog):
            self.ui.gpuComboBox.setCurrentIndex(1)
            self.ui.gpuComboBox.setEnabled(False)
            self.ui.text1.setPlainText("gpu not available\n")
-           self.text_edit.moveCursor(QTextCursor.End)
 
         self.ui.qualityComboBox.addItem("small")
         self.ui.qualityComboBox.addItem("middle")
         self.ui.qualityComboBox.addItem("best")
-        self.ui.qualityComboBox.setCurrentIndex(1)
-                
+        self.ui.qualityComboBox.setCurrentIndex(1)                
 
         self.fov_x=None
         self.max_depth_m = 100
@@ -159,29 +152,15 @@ class SettingsDlg(QDialog):
             print("use cpu")
         
         model = MoGeModel.from_pretrained(modelPath).to(device)
-        #model.max_depth_m = 100
+        
         end = time.time()
         print(f"model = MoGeModel.from_pretrained: {end - start:.2f} sec")
 
         self.ui.text1.setPlainText(f"model = MoGeModel.from_pretrained: {end - start:.2f} sec\n")
         self.ui.text1.moveCursor(QTextCursor.End)
-
-        # Read the input image and convert to tensor (3, H, W) with RGB values normalized to [0, 1]
+                
         input_image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB) 
-        #h,w = input_image.shape[:2]
-        #newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, (w, h), 0, (w, h))
-        #input_image_undst = cv2.undistort(input_image, camera_matrix, dist_coefs, None, newcameramtx)
-        #input_image_undst = input_image
         input_image_t = torch.tensor(input_image / 255, dtype=torch.float32, device=device).permute(2, 0, 1)    
-
-
-        #fov_1 = math.degrees(math.atan(float(2592)/float(5800)))*2
-        #fov_1 = 1
-
-        #diag = math.sqrt(h**2+w**2)
-        #fov_1 = math.degrees(math.atan(float(diag/2)/float(5800)))*2
-        #fov_1 = 55
-        # Infer 
         
         if(self.fov_x is not None):
             self.fov_x = float(self.ui.fov_x.text())
@@ -212,7 +191,7 @@ class SettingsDlg(QDialog):
                 use_fp16=False,
                 max_depth_m=self.max_depth_m,
                 metric_scale_mnoj = self.metric_scale_mnoj)
-        #output = model.infer(input_image_t)
+        
         points, depth, mask, intrinsics, focal_X, fx, fy  = output['points'].cpu().numpy(), output['depth'].cpu().numpy(), output['mask'].cpu().numpy(), output['intrinsics'].cpu().numpy(),  output['focal'].cpu().numpy(),  output['fx'].cpu().numpy(),  output['fy'].cpu().numpy()
         normal = output['normal'].cpu().numpy()
         end = time.time()
@@ -228,17 +207,13 @@ class SettingsDlg(QDialog):
 
         start = time.time()
         cv2.imwrite(Path.joinpath(output_dir,img_name_without_ext+".png"), cv2.cvtColor(colorize_depth(depth), cv2.COLOR_RGB2BGR))
-        cv2.imwrite(Path.joinpath(output_dir,img_name_without_ext+".exr"), depth, [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_FLOAT])
-        #cv2.imwrite(str(save_path + 'depth.exr'), depth, [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_FLOAT])
+        cv2.imwrite(Path.joinpath(output_dir,img_name_without_ext+".exr"), depth, [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_FLOAT])        
         cv2.imwrite(Path.joinpath(output_dir,'mask.png'), (mask * 255).astype(np.uint8))
 
         self.Ui_MainWindow.gView2.addImage(Path.joinpath(output_dir,img_name_without_ext+".png"))
         self.Ui_MainWindow.gView2.addEXR(Path.joinpath(output_dir,img_name_without_ext+".exr"),focal_X, fx, fy)
         self.Ui_MainWindow.gView1.addEXR(Path.joinpath(output_dir,img_name_without_ext+".exr"),focal_X, fx, fy)
         
-
-        #cv2.imwrite(str(save_pathtntdjqtntdPA110016 + 'points.exr'), cv2.cvtColor(points, cv2.COLOR_RGB2BGR), [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_FLOAT])
-        #cv2.imwrite(str(save_path + 'points.jpg'), cv2.cvtColor(points, cv2.COLOR_RGB2BGR), [cv2.IMWRITE_JPEG_QUALITY, 100])
         end = time.time()
         print("Save time with no ply "+img_name+f" {end - start:.2f} sec")
 
@@ -260,11 +235,7 @@ class SettingsDlg(QDialog):
         save_ply(Path.joinpath(output_dir,img_name_without_ext+".ply"), vertices, np.zeros((0, 3), dtype=np.int32), vertex_colors, vertex_normals)
 
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-        #input_image_undst = cv2.cvtColor(input_image_undst, cv2.COLOR_BGR2RGB)
         cv2.imwrite(Path.joinpath(output_dir,mPATH.name), input_image)
-        #cv2.imwrite(Path.joinpath(output_dir,'ishodnik_undist.jpg'), input_image_undst)
-
-        
 
         print("ply file saved")
         self.ui.calcButton.setEnabled(True)
